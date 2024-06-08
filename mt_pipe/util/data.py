3c8321418e16a02de257647ae7ce29d4aa55ee28
Avishka Perera <pereramat2000@gmail.com>
2024-06-08 08:09:00 +0530

    added concat dataset

--- mt_pipe/util/data.py
+++ mt_pipe/util/data.py
@@ -1,9 +1,13 @@
-"""Utility functions related to data"""
+"""Utilities related to data"""
 
 from typing import Sequence, Callable, Dict
 
 import torch
 from torch.utils.data import default_collate
+from torch.utils.data import ConcatDataset, Dataset
+from omegaconf import OmegaConf, ListConfig
+
+from .util import make_obj_from_conf
 
 
 def to_device_deep(obj, device):
@@ -24,3 +28,28 @@ def get_collate_func(augmentor) -> Callable:
         return batch
 
     return collate_fn
+
+
+class ConcatSet(Dataset):
+    def __init__(self, conf: Sequence[Dict] | ListConfig = []) -> None:
+        assert conf != []
+        assert all(["target" in comp_conf for comp_conf in conf])
+
+        conf = OmegaConf.create(conf)
+
+        datasets = []
+        for ds_conf in conf:
+            ds = make_obj_from_conf(ds_conf)
+            if "reps" in ds_conf:
+                reps = ds_conf.reps
+            else:
+                reps = 1
+            datasets.extend([ds] * reps)
+
+        self.dataset = ConcatDataset(datasets)
+
+    def __len__(self) -> int:
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        return self.dataset.__getitem__(index)
