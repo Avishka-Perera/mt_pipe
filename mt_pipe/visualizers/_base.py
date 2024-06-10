a6eba6c456ca6375118cac166c6cddd08c9978de
Avishka Perera <pereramat2000@gmail.com>
2024-06-10 09:35:53 +0530

    base visualizer bug fix

--- mt_pipe/visualizers/_base.py
+++ mt_pipe/visualizers/_base.py
@@ -9,10 +9,12 @@ import matplotlib.pyplot as plt
 
 class Visualizer:
     def __init__(
-        self, _writer, name: str = None, max_imgs_per_batch: int = None
+        self,
+        name: str = None,
+        max_imgs_per_batch: int = None,
+        _writer: SummaryWriter = None,
     ) -> None:
         self.name = self.__class__.__name__ if name is None else name
-        self.writer: SummaryWriter = None
         self.max_imgs_per_batch = max_imgs_per_batch
         self.global_step = 0
         self.writer = _writer
